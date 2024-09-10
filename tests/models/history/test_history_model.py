ac89c294d8e38ca0aa3756f77582f2605734082a
thercistenes <thercistenes1@outlook.com>
2024-09-10 10:53:39 -0300

    cria teste para request_history

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,31 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
-# Req. 7
 def test_request_history():
-    raise NotImplementedError
+    response = HistoryModel.list_as_json()
+    response_data = json.loads(response)
+
+    expected_entries = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt"
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt"
+        }
+    ]
+
+    assert len(response_data) == len(expected_entries)
+
+    for expected_entry in expected_entries:
+        assert any(
+            entry["text_to_translate"] == expected_entry
+            ["text_to_translate"] and
+            entry["translate_from"] == expected_entry["translate_from"] and
+            entry["translate_to"] == expected_entry["translate_to"]
+            for entry in response_data
+        )
